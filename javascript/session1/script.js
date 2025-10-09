ecacfb591ffb276fa74c4b9ad24746edad40f16d
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-09 19:43:58 +0330

    ss

--- javascript/session1/script.js
+++ javascript/session1/script.js
@@ -1,50 +1,17 @@
-let username = document.querySelector('.username')
-let password = document.querySelector('.password')
-let modal = document.querySelector('.modal')
-let button = document.querySelector('button')
+let countrySelect = document.querySelector('.countrySelect')
+let citySelect = document.querySelector('.citySelect')
 
-
-button.addEventListener('click', 
-    function dataValidation(){
-    let usernameVal = username.value
-    let passwordVal = password.value
-
-    if(usernameVal.length < 12 || passwordVal.length < 8){
-        modal.style.background = 'red'
-        modal.innerHTML = "اطلاعات نادرست است"
-        modal.style.display = 'inline'
-    }else{
-        modal.style.background = 'green'
-        modal.innerHTML = "اطلاعات درست است"
-        modal.style.display = 'inline'
-
-        password.classList.add("red")
-
-    }
-
-    setTimeout(function(){
-        modal.style.display = "none"
-    }, 3000)
-}
-)
-
-
-function myFocus(){
-    console.log("focus")
-    username.style.border = "2px solid red"
+let cdata = {
+    Iran: ['tehran', 'zahedan'],
+    Turkey: ['ist', 'ez']
 }
 
-function myBlur(){
-    console.log("blur")
-    username.style.border = ""
-}
+countrySelect.addEventListener('change', function(){
+    let selected = countrySelect.value
+    let cities = cdata[selected]
+    citySelect.innerHTML = ""
 
-function MydblClick(){
-    console.log("blalal")
-}
-
-
-let s = document.getElementById("countries")
-function changeHandler(){
-    console.log(s.value)
-}
+    cities.forEach(function(city){
+        citySelect.innerHTML += `<option>${city}</option>`
+    });
+})
