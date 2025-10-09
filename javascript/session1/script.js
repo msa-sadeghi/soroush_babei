7fa7de362a46a0614ae9ed54b8bdfbb712545c3b
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-09 19:09:23 +0330

    init

--- javascript/session1/script.js
+++ javascript/session1/script.js
@@ -1,137 +1,32 @@
-// var x = 4
-// var y = 7
-// var z = 2
-
-// if(x < y){
-//     console.log(`${x} is smaller than ${y}`)    
-// }else if(x > y){
-
-// }else{
-
-// }
-
-// سه عدد را از کاربر دریافت بکنید
-//  و میانگین آن ها را محاسبه کرده و به کاربر نمایش بدین
+let img = document.getElementsByTagName('img')[0]
+let btn = document.getElementsByTagName('btn')[0]
 
 
+let flag = false
+function turnOnOff(){
+    if(flag){
+        img.setAttribute('src', 'bulboff.gif')
+        flag = false
+    }
+    else{
+        img.setAttribute('src', 'bulbon.gif')
+        flag = true
 
-// سن کاربر را دریافت کنید.
-// اگر سن کاربر کمتر از 18 بود، به او پیغام "شما مجاز به ورود نیستید"
-//  را نمایش بدهید و در غیر این صورت پیغام "ورود به پنل" 
-// را نمایش بدهید.
-
-
-// var userBirth = prompt("Enter your birth date", 1367)
-// console.log(userBirth)
-// var userAge = 1404 - userBirth
-
-// if(isNaN(userAge)){
-
-// }else{
-
-// }
-
-// or    ||
-// and    &&
-// !true
-// از کاربر سن و جنسیت را دریافت کنید.
-// در صورتی که جنسیت کاربر مونث بوده
-//  یا سن کمتر از 18 را دارد، به اون خطا نمایش داده
-// و در غیر این صورت به اون مجوز ورود به پنل را نمایش دهید.
-
-// var x = 2;
-// var y = 2;
-
-// // x * y === 4 ? console.log("*") :  console.log("-")
+    }
+}
 
 
-// var z = x *y
+// یک فرم لاگین پیاده سازی کنید
+//  که از 2 اینپوت و 1 دکمه تشکیل شده باشد
+
+// به محض کلیک روی دکمه فرم، ولیوی اینپوت‌ها بررسی شوند 
+// و اگر با محدودیت های لازم مطابقت نداشتند،
+//  به کاربر پیغام خطا نمایش دهید 
+// و در غیر این صورت پیغام مناسبی را نمایش دهید
 
 
-// switch(z){
-//     case 18:
-//     case 19:
-//     case 20:
-//         console.log("A")
-//         break
-//     case 15:
-//     case 16:
-//     case 17:
-//         console.log("B")
-//         break
-    
+// اینپوت یوزرنیم حداقل باید 12 کاراکتر
+//  و اینپوت پسورد حداقل باید 8 کاراکتر داشته باشد
 
-
-//     default:
-//         console.log("")
-//         break
-// }
-
-
-
-
-
-// معدل کاربر را دریافت کرده و سپس سطح او را تعیین کنید
-
-// A برای معدل 18 الی 20 سطح
-// B برای معدل 15 الی 17 سطح
-// C برای معدل 12 الی 14 سطح
-// معدل کمتر از 12 مشروط میشه :(
-
-
-
-
-
-// function myFunction(){
-    //     return "hello"
-    // }
-    
-    
-// const myFunction = function(){
-//     return "hello"
-// }
-// console.log(myFunction())
-
-
-// تابعی بنویسید که عددی را دریافت کرده
-//  و زوج یا فرد بودن آن را نمایش دهد
-
-
-// تابعی بنویسید که سه عدد رو بگیره
-//  و میانگین اونا رو محاسبه کرده و نشون بده
-
-
-
-// var numbers = [134,34]
-// var names = "salam"
-
-// console.log(numbers.length)
-// console.log(names.length)
-// console.log(typeof numbers)
-// console.log(Array.isArray(numbers))
-// numbers.push(12)
-// console.log(numbers)
-// var index = numbers.indexOf(34)
-// numbers.splice(index, 1)
-// console.log(numbers)
-
-
-var test = "this is a test "
-
-console.log(test.slice(0, 4))
-console.log(test.substring(0, 4))
-
-
-// test.trim().toLowerCase()
-// console.log(test[0])
-// console.log(test.charAt(2))
-// console.log(test.charCodeAt(2)
-// console.log(test.search("test"))
-// console.log(test.indexOf("test"))
-// console.log(test.includes("test"))
-// var test2 = "blalalal"
-
-// var res = test + ['kkkk', "hhhh"]
-// console.log(res)
-// var res2 = test.concat(["bbb", "cccc"])
-// console.log(res2)
+// نکته: برای خوشگل‌تر شدن پروژه، برای نمایش پیغام 
+// های مورد نیاز به جای آلرت از مُدال استفاده کنید
