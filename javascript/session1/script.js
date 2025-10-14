3ae67bc851be6cc96b3d95cc0df6b30fca984ee6
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-14 17:04:29 +0330

    s

--- javascript/session1/script.js
+++ javascript/session1/script.js
@@ -20,7 +20,18 @@ button.addEventListener('click', function(){
     })
 
     if(citySelected){
-        
+
         weather.classList.remove('loading')
     }
 })
+
+
+function f1(){
+    console.log("f1")
+}
+function f2(){
+    console.log("f2")
+}
+function f3(){
+    console.log("f3")
+}
