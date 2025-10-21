1a097bb379781d7338384ce6247ecd7925f3fd45
mostafa mostafa <155224744+msa-sadeghi@users.noreply.github.com>
2025-10-21 17:06:35 +0330

    ss

--- javascript/session1/script.js
+++
@@ -1,8 +0,0 @@
-// let head3 = document.getElementById("head3")
-
-// console.log(head3.nextSibling)
-
-
-let listItems= document.getElementById("list")
-console.log(list.childElementCount)
-console.log(list.childNodes)
