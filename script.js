92919d108659aa81c11a9747f89e518c48055712
msa-sadeghi <sadeghi.msai@gmail.com>
2025-10-28 17:45:43 +0330

    codes

---
+++ script.js
@@ -0,0 +1,7 @@
+const loaderElement = document.querySelector('.loader')
+
+window.addEventListener('load', function(){
+    loaderElement.className = "loader hidden"
+    loaderElement.classList.add("hidden")
+})
+
