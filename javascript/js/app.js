e7de8e38fd32761c954b2e2614b959b80442ae52
msa-sadeghi <sadeghi.msai@gmail.com>
2025-11-04 17:19:44 +0330

    codes

---
+++ javascript/js/app.js
@@ -0,0 +1,27 @@
+let $ = document
+const players = $.querySelectorAll('.fa-play')
+const audios = $.querySelectorAll('audio')
+
+
+console.log(audios[0])
+
+
+// let musicName;
+
+// players.forEach(function (player) {
+
+//     player.addEventListener('click', function (event) {
+//         musicName = event.target.dataset.name
+
+//         audios.forEach(function (audio) {
+//             if (audio.dataset.name === musicName) {
+//                 audio.currentTime = 0
+//                 audio.play()
+//             } else {
+//                 audio.pause()
+//             }
+//         })
+
+//     })
+
+// })
